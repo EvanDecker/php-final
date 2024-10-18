aefeff635e5d0937f0c7594e6cb2b8f51ff5755f
EvanDecker <evan.decker@yahoo.com>
2024-10-18 13:42:03 -0500

    model first pass

--- models/models.php
+++ models/models.php
@@ -2,6 +2,17 @@
 namespace Models;
 
 class Book {
+  // CRUD books interacting w/DB
+  // find() and findAll() to fetch model records
+
+  // save() to insert or update a record into the db
+    // attempt to validate the model before trying to save
+      // if not validated, should return False
+    // return True or False if successful
+    
+  // destroy() to delete the record from the db
+  // validate to validate properties on the model (returns True or False as to if the model is valid)
+    // should feed data to an errors() function that returns an array of errors (if any)
   function __construct(string $title, string $author, int $totalPages, int $pagesRead = 0) {
     $this->$title = $title;
     $this->$author = $author;
