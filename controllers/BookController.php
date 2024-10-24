f9315bfac58db99b0ca9df537055ec655c9b7e7c
EvanDecker <evan.decker@yahoo.com>
2024-10-24 10:59:16 -0500

    fix indentation

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -4,15 +4,15 @@ use App\Models\Book;
 require_once '../models/models.php';
 
 class BookController {
-  function __construct($model){
-    $this->model = $model;
-  }
+    function __construct($model){
+        $this->model = $model;
+    }
 
-  public static function render() {
-    $BookModel = new Book;
-    $BookController = new BookController($BookModel);
-    $books = $BookController->model->findAll();
-    require_once '../views/books.php';
-  }
-  public $model;
+    public static function render() {
+        $BookModel = new Book;
+        $BookController = new BookController($BookModel);
+        $books = $BookController->model->findAll();
+        require_once '../views/books.php';
+    }
+    public $model;
 }
--- views/books.php
+++ views/books.php
@@ -4,8 +4,6 @@ namespace App\Views;
 <!DOCTYPE html>
 <html lang="en">
 <body>
-  <h1>Book App</h1>
-  <?php
-    require_once '../views/bookDisplay.php';
-  ?>
+    <h1>Book App</h1>
+    <?php require_once '../views/bookDisplay.php'; ?>
 </body>
