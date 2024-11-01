4060bc291305aaf9d859b6fcaf1dc328fa6ed849
EvanDecker <evan.decker@yahoo.com>
2024-11-01 09:09:00 -0500

    fix mysql setup on container

--- init.sql
+++ init.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE IF NOT EXISTS modules;
 use mysql;
-DROP USER 'modules'@'%';
-CREATE USER 'modules'@'172.23.0.3' IDENTIFIED BY 'secret';
-GRANT ALL ON *.* TO 'modules'@'172.23.0.3';
+GRANT ALL ON *.* TO 'modules'@'%';
 use modules;
 CREATE TABLE IF NOT EXISTS books (id INT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255), author VARCHAR(255), pages INT);
