b25f47b2b8e8b7478beebb8828207e3999b052f2
EvanDecker <evan.decker@yahoo.com>
2024-11-04 09:05:50 -0600

    remove comment

--- Database/Database.php
+++ Database/Database.php
@@ -12,8 +12,6 @@ class Database
         $username = "root";
         $password = "secret";
         try {
-            //TODO: figure out and fix having to change `host.docker.internal` to `localhost` when running the tests ????
-            //pretty sure it's an issue regarding the docker network / not being able to find the docker container from the test
             $connection = new PDO("mysql:host=mysql:3306;dbname=$dbname", $username, $password);
             return $connection;
         } catch (PDOException $e) {
