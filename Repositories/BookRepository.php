1a65c8baf88bd82ef4e2303c2290f0068af416f1
EvanDecker <evan.decker@yahoo.com>
2024-11-04 10:27:05 -0600

    repo documented

--- Repositories/BookRepository.php
+++ Repositories/BookRepository.php
@@ -6,63 +6,136 @@ require_once __DIR__ . '/BookRepositoryInterface.php';
 
 class BookRepository implements BookRepositoryInterface
 {
+    /** The model that interacts with the DB. */
+    private $model;
+
     public function __construct($model)
     {
         $this->model = $model;
     }
 
-    private $model;
-
+    /**
+     * Calls the model's find method.
+     * 
+     * @param mixed $id The id of the book to be found.
+     * 
+     * @return mixed The result as an array of objects.
+     */
     public function find($id)
     {
         return $this->model->find($id);
     }
 
+    /**
+     * Calls the model's findAll method.
+     * 
+     * @return mixed The results as an array of objects.
+     */
     public function findAll()
     {
         return $this->model->findAll();
     }
 
+    /**
+     * Calls the model's create method.
+     * 
+     * @param mixed $book The book to be added to the DB.
+     * 
+     * @return mixed The success of the operation.
+     */
     public function create($book)
     {
         return $this->model->save($book);
     }
 
+    /**
+     * Calls the model's update method.
+     * 
+     * @param mixed $book The book to be updated.
+     * @param mixed $update Flag to indicate an update instead of an insert.
+     * 
+     * @return mixed The success of the operation.
+     */
     public function update($book, $update)
     {
         return $this->model->save($book, $update);
     }
 
+    /**
+     * Calls the model's destroy method.
+     * 
+     * @param mixed $id The ID of the book to be deleted.
+     * 
+     * @return mixed The success of the operation.
+     */
     public function destroy($id)
     {
         return $this->model->destroy($id);
     }
 
+    /**
+     * Calls the model's findByTitle method.
+     * 
+     * @param string $title The title of the book to be found.
+     * 
+     * @return mixed The results as an array of objects.
+     */
     public function findByTitle($title)
     {
         return $this->model->findByTitle($title);
     }
 
+    /**
+     * Calls the model's errors method.
+     * 
+     * @return mixed The array of errors.
+     */
     public function errors()
     {
         return $this->model->errors();
     }
 
+    /**
+     * Calls the model's addError method.
+     * 
+     * @param mixed $err The error to be added.
+     * 
+     * @return void
+     */
     public function addError($err)
     {
         $this->model->addError($err);
     }
 
+    /**
+     * Calls the model's removeError method.
+     * 
+     * @param mixed $err The error to be removed.
+     * 
+     * @return void
+     */
     public function removeError($err)
     {
         $this->model->removeError($err);
     }
 
+    /**
+     * Calls the model's checkErrInArr method.
+     * 
+     * @param mixed $err The error to be checked.
+     * 
+     * @return void
+     */
     public function checkErrInArr($err)
     {
         $this->model->checkErrInArr($err);
     }
 
+    /**
+     * Calls the model's validate method.
+     * 
+     * @return mixed The success of the operation.
+     */
     public function validate()
     {
         return $this->model->validate();
